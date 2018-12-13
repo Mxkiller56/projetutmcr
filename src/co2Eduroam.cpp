911c46c9a238e098588b1541989b2a07796b087e
ugo <43913579+ulecrivain@users.noreply.github.com>
2018-12-13 17:24:05 +0100

    removed duplicated file adapted co2Eduroam code, added timeout

--- src/co2Eduroam.cpp
+++ src/co2Eduroam.cpp
@@ -1,43 +1,31 @@
-//Sketch edited by: martinius96 (Martin Chlebovec)
-//Personal website: https://arduino.php5.sk
 #include "esp_wpa2.h"
 #include <WiFi.h>
-#define EAP_IDENTITY "" //eduroam login --> identity@youruniversity.domain
-#define EAP_PASSWORD "" //your Eduroam password
-String line; //variable for response
-const char* ssid = "eduroam"; // Eduroam SSID
-const char* host = ""; //external server domain for HTTP connection after authentification
-void connexion(){
-  delay(10);
-  Serial.println();
-  Serial.print("Connecting to network: ");
+
+bool _802_1x_eap_connect(char *ssid, char *identity, char *password){
   Serial.println(ssid);
   WiFi.disconnect(true);  //disconnect form wifi to set new wifi connection
   WiFi.mode(WIFI_STA);
-  esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY)); //provide identity
-  esp_wifi_sta_wpa2_ent_set_username((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY)); //provide username
-  esp_wifi_sta_wpa2_ent_set_password((uint8_t *)EAP_PASSWORD, strlen(EAP_PASSWORD)); //provide password
+  esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)identity, strlen(identity)); //provide identity
+  esp_wifi_sta_wpa2_ent_set_username((uint8_t *)identity, strlen(identity)); //provide username
+  esp_wifi_sta_wpa2_ent_set_password((uint8_t *)password, strlen(password)); //provide password
   esp_wpa2_config_t config = WPA2_CONFIG_INIT_DEFAULT(); //set config to default (fixed for 2018 and Arduino 1.8.5+)
   esp_wifi_sta_wpa2_ent_enable(&config); //set config to enable function (fixed for 2018 and Arduino 1.8.5+)
   WiFi.begin(ssid); //connect to Eduroam function
-  WiFi.setHostname("ESP32Name"); //set Hostname for your device - not neccesary
-  while (WiFi.status() != WL_CONNECTED) {
+  unsigned long next_timeout = millis() + 10000L; // 10 seconds
+  while (WiFi.status() != WL_CONNECTED && millis() < next_timeout) {
     delay(500);
+#ifdef DEBUG
     Serial.print(".");
+#endif
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address set: ");
-  Serial.println(WiFi.localIP()); //print LAN IP
+  if (WiFi.status() == WL_CONNECTED){
+#ifdef DEBUG
+    Serial.println("WiFi connected");
+    Serial.println("IP address set: ");
+    Serial.println(WiFi.localIP()); //print LAN IP
+#endif
+    return true;
+  } else
+    return false; // we had a problem connecting
 }
 
-void setup() {
-  Serial.begin(9600);
-  connexion();
-  delay(250);
-}
-void loop() {
-  Serial.println("IP address set: ");
-  Serial.println(WiFi.localIP()); //print LAN IP
-  delay(1000);
-}
